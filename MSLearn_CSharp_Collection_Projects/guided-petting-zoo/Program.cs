f24b51fc6ea52f3a02934e2e1eed353859351bf7
Kristy Gray <thekristyleigh@gmail.com>
2023-10-27 12:10:08 +0800

    Add pseudo code to plan methods and data types

--- MSLearn_CSharp_Collection_Projects/guided-petting-zoo/Program.cs
+++ MSLearn_CSharp_Collection_Projects/guided-petting-zoo/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/*
+
+    DESIGN SPECIFICATIONS
+
+    - There will be three visiting schools
+        - School A has six visiting groups (the default number)
+        - School B has three visiting groups
+        - School C has two visiting groups
+
+    - For each visiting school, perform the following tasks
+        1. Randomize the animals
+        2. Assign the animals to the correct number of groups
+        3. Print the school name
+        4. Print the animal groups
+
+*/
+
+using System;
 
 string[] pettingZoo = 
 {
@@ -6,3 +23,9 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
+
+// RandomizeAnimals();
+// string[,] group = AssignGroup();
+
+Console.WriteLine("School A");
+// PrintGroup(group);
