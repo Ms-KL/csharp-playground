4fe9948e2c3a86b9aef4198ca4b364bed921e067
Kristy Gray <thekristyleigh@gmail.com>
2023-10-30 14:43:15 +0800

    Complete walkthough of project

--- MSLearn_CSharp_Collection_Projects/guided-petting-zoo/Program.cs
+++ MSLearn_CSharp_Collection_Projects/guided-petting-zoo/Program.cs
@@ -24,10 +24,18 @@ string[] pettingZoo =
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
 
-RandomizeAnimals();
-// string[,] group = AssignGroup();
-Console.WriteLine("School A");
-// PrintGroup(group);
+// call methods - required = schoolName, optional = groups
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
+
+void PlanSchoolVisit(string schoolName, int groups = 6)
+{
+    RandomizeAnimals();
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
+}
 
 void RandomizeAnimals() 
 {
@@ -43,6 +51,50 @@ void RandomizeAnimals()
     }
 }
 
+string[,] AssignGroup(int groups = 6)
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    // groups = the number of animal groups you want to create. 
+    // pettingZoo.Length/groups = how many animals are assigned to each group. 
+    // For example, since pettingZoo is a fixed array of 18 elements, the 2D array size for School A is [6,3].
+    
+    int start = 0;
+    
+    // assign animal to each group
+    for (int i = 0; i < groups; i++) 
+    // for group in groups
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        // for animal in group
+        {
+            result[i,j] = pettingZoo[start++];
+            // result = name of group and animal
+        }
+    }
+
+    return result;
+    
+}
+
+void PrintGroup(string[,] group)
+// parameter = 2d string array named group
+{
+    for (int i = 0; i < group.GetLength(0); i++) 
+    // for group in groups in individual school
+    {
+        Console.Write($"Group {i + 1}: ");
+        // print group number        
+        for (int j = 0; j < group.GetLength(1); j++) 
+        // for animal in group
+        {
+            Console.Write($"{group[i,j]}  ");
+            // print each group then animal
+        }
+        Console.WriteLine();        
+    }
+}
+
+// ------------------------------------
 // test
 
 // foreach(string animal in pettingZoo) 
