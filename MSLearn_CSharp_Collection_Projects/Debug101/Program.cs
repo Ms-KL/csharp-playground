d35891882f02b7fdb7cc0913bce6548e3f8628fe
Kristy Gray <thekristyleigh@gmail.com>
2023-11-02 11:23:56 +0800

    Complete Unit 8 and setup challenge

--- MSLearn_CSharp_Collection_Projects/Debug101/Program.cs
+++ MSLearn_CSharp_Collection_Projects/Debug101/Program.cs
@@ -1,80 +1,14 @@
-﻿// This code uses a method named LoadProducts to load data into the products array. 
-// After the data is loaded, the code iterates through the array and calls methods named Process1 and Process2.
+﻿/*  
+This code instantiates a value and then calls the ChangeValue method
+to update the value. The code then prints the updated value to the console.
+*/
+int x = 5;
 
-int productCount = 2000;
-string[,] products = new string[productCount, 2];
+ChangeValue(x);
 
-LoadProducts(products, productCount);
+Console.WriteLine(x);
 
-for (int i = 0; i < productCount; i++)
+void ChangeValue(int value) 
 {
-    string result;
-    result = Process1(products, i);
-
-    if (result != "obsolete")
-    {
-        result = Process2(products, i);
-    }
-}
-
-bool pauseCode = true;
-while (pauseCode == true) ;
-
-// generate data for the simulated processes
-
-// The LoadProducts method generates 2000 random product IDs and assigns a value of existing, new, or obsolete to a product / description field. There is about a 1% chance that the products are marked new.
-
-static void LoadProducts(string[,] products, int productCount)
-{
-    Random rand = new Random();
-
-    for (int i = 0; i < productCount; i++)
-    {
-        int num1 = rand.Next(1, 10000) + 10000;
-        int num2 = rand.Next(1, 101);
-
-        string prodID = num1.ToString();
-
-        if (num2 < 91)
-        {
-            products[i, 1] = "existing";
-        }
-        else if (num2 == 91)
-        {
-            products[i, 1] = "new";
-            prodID = prodID + "-n";
-        }
-        else
-        {
-            products[i, 1] = "obsolete";
-            prodID = prodID + "-0";
-        }
-
-        products[i, 0] = prodID;
-    }
-}
-
-// simulate data processing
-
-// The Process1 and Process2 methods display progress messages and return a string. Notice that the Process2 method calls Process3 if the product is new.
-
-static string Process1(string[,] products, int item)
-{
-    Console.WriteLine($"Process1 message - working on {products[item, 1]} product");
-
-    return products[item, 1];
-}
-
-static string Process2(string[,] products, int item)
-{
-    Console.WriteLine($"Process2 message - working on product ID #: {products[item, 0]}");
-    if (products[item, 1] == "new")
-        Process3(products, item);
-
-    return "continue";
-}
-
-static void Process3(string[,] products, int item)
-{
-    Console.WriteLine($"Process3 message - processing product information for 'new' product");
+    value = 10;
 }
